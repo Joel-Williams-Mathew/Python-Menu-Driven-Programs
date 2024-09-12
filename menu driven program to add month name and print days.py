e0ae0ee3825206d19ad5dc09d3b696865a93f29e
Joel Williams Mathew <joelw2200@gmail.com>
2024-09-12 06:45:25 +0530

    Add files via upload

---
+++ menu driven program to add month name and print days.py
@@ -0,0 +1,43 @@
+d={}
+monthname="January,February,March,April,May,June,July,August,September,October,November,December"
+days="31,28,31,30,31,30,31,31,30,31,30,31"
+d[monthname]=days
+print(d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"a=input(Enter the Month Name :"
+"for i in d:"
+"if a==d:"
+"print(d)"
+
+
+
+
+
+
+""
+
+
+
+
+
+
+
+
+
+
+
+
+{"January":31,"February":28,"March":31,"April":30,"May":31,"June":30,"July":31,"August":31,"September":30,"October":31,"November":30,"December":31}
